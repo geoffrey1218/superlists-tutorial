5be4f0b67f9d0cf046ab0378b8abdb75ae4ab8fe
geoffrey1218 <geoff.s.rich@gmail.com>
2018-02-02 18:00:53 -0800

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e&xaxc-g39*!cz3p==g(2jk8ua-rmy&9qo9440-c&xa_ky9v7u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
